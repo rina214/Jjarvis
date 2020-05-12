193957b96d627086da7563f9e10bc6fd6cbcdbf5
jiyoonlee <sundaeluv98@gmail.com>
2020-05-13 02:34:43 +0900

    2020-05-13(수) 로그인창 화면전환만 완성 (서버 DB 연동 X)
    //해야할 것
    1. 프레그먼트 뒤로가기를 안드로이드 뒤로가기 자체로 되게 하는거 공부.
    2. 키보드 사용시와 사용 안할떄 화면 적용다르게 하기.
    3. 기기가 바뀌더라도 화면 비율 적용 시키기.
    4. 기능 추가 정리 하기 (ex. 2,3,4,5프레임 & 로그인창 DB 기능 추가)
    5. (시간 남으면) 디자인 구상하기.

--- app/src/main/java/com/example/jjarvis/MainActivity.java
+++ app/src/main/java/com/example/jjarvis/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.jjarvis;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt_sign_in;
+    Button bt_sign_up;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //회원가입에서 넘어가는 버튼
+        bt_sign_in=(Button)findViewById(R.id.sign_in_button);
+        bt_sign_up=(Button)findViewById(R.id.sign_up_button);
+
+
+        bt_sign_in.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bt_sign_up.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
 }
